49c6c637043ccbbbd2e2afd3b3a686517c12277a
Madhu <68181437+joebeck20@users.noreply.github.com>
2021-05-14 19:32:24 +0530

    Register Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,8 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import SigninScreen from './screens/SigninScreen';
+import RegisterScreen from './screens/RegisterScreen';
+
 
 
 
@@ -62,6 +64,7 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,10 @@ import {
     productListReducer,
   } from './reducers/productReducers';
 
-import { userSigninReducer } from './reducers/userReducers';
+import {
+  userRegisterReducer,
+  userSigninReducer,
+} from './reducers/userReducers';
 
   const initialState = {
     userSignin: {
@@ -27,6 +30,7 @@ import { userSigninReducer } from './reducers/userReducers';
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userSignin: userSigninReducer,
+  userRegister: userRegisterReducer,
 });
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
